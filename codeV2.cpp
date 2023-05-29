fb6e74a9fc0771ba6120fe94b711f6fb43318275
Abyseal <115791417+Abyseal@users.noreply.github.com>
2023-05-29 23:00:41 +0300

    Update codeV2.cpp

--- codeV2.cpp
+++ codeV2.cpp
@@ -19,6 +19,8 @@ void barber();
 void customer(int number);
 
 int main() {
+	setlocale(LC_ALL, "rus");
+	
 	std::thread barb(barber);
 	std::thread cust1(customer,5);
 	std::thread cust2(customer, 6);
@@ -43,10 +45,10 @@ void barber() {
 		if (barbershopQueue.size() > 0) { // поток продолжается когда барбера разбудили, т.е пришел клиент
 			int human = barbershopQueue.front(); //приглашается человек, который дольше всего ждет (исключение проблемы starvation)
 			barbershopQueue.pop();
-			std::cout << "Barber is cutting hair for " << human << " customer" << std::endl;
-			
+			std::cout << "Barber is cutting hair for customer №" << human << std::endl;
 			std::this_thread::sleep_for(std::chrono::seconds(3)); //процесс стрижки
-			std::cout << "Barber finished " << human << " customer" << std::endl;
+			std::cout << "Barber has finished cutting hair for customer №" << human << std::endl;
+			std::cout << "Barber-chair is now available" << std::endl;
 			barberRoomStatus.release(); //Клиент подстригся, комната освобождается
 		}
 	}
@@ -57,18 +59,20 @@ void customer(int number) {
 	barbershopDoor.lock(); // клиент вошел в зал ожидания, открыл входную дверь. В это время никто больше не может войти, исключение проблемы, когда 2 клиента могут сесть на одно место
 	if (barbershopQueue.size() < SEATS_NUMBER) { // Если есть свободное место, то клиент становится в очередь/садится на свободный стул
 		barbershopQueue.push(number); 
-		std::cout << "Customer " << number << " is waiting in the queue" << std::endl;
+		std::cout << "Customer №" << number << " is waiting in the queue" << std::endl;
 		barbershopDoor.unlock(); // когда клиент сел входная дверь "освободилась" теперь следующий клиент сможет войти
 
 		barberRoomDoor.lock(); // Клиент пытается войти в комнату барбера.Если комната уже занята, то поток блокируется (посетитель ждет).
+		std::cout << "Customer №" << number << " entered barber room" << std::endl;
 		barberStatus.release(); // Когда клиент смог попасть в комнату, он "будит" барбера, если тот спал или просто садится в кресло
+		std::cout << "Barber chair is now occupied" << std::endl;
 		barberRoomStatus.acquire(); // Клиент занимает кресло
 		
-		std::cout << "Customer " << number << " has finished" << std::endl;
+		std::cout << "Customer №" << number << " has finished getting a haircut and left the barbershop " << std::endl;
 		barberRoomDoor.unlock(); //Клиент выходит из комнаты барбера
 	}
 	else {
-		std::cout << "Customer " << number << " is leaving" << std::endl; // Если мест в зале ожидания нет, то клиент уходит
+		std::cout << "Customer №" << number << " is leaving, because waiting room is full" << std::endl; // Если мест в зале ожидания нет, то клиент уходит
 		barbershopDoor.unlock();
 	}
 }
