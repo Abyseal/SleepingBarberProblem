46f89db22d686d0d6ab493237cf89e6234e5803e
Abyseal <115791417+Abyseal@users.noreply.github.com>
2023-05-29 22:41:44 +0300

    Update codeV2.cpp

--- codeV2.cpp
+++ codeV2.cpp
@@ -5,15 +5,15 @@
 #include <chrono>
 #include <mutex>
 
-int CUSTOMERS_NUMBER = 5;
-int SEATS_NUMBER = 2;
+int CUSTOMERS_NUMBER = 5; //число посетителей
+int SEATS_NUMBER = 2; // число стульев в комнате ожидания
 
-std::binary_semaphore barberRoomStatus(0);
-std::counting_semaphore barberStatus(0);
+std::binary_semaphore barberRoomStatus(0); //эмитация кресла
+std::counting_semaphore barberStatus(0); //<0 свободен,=0 занят
 std::mutex barberRoomDoor; //mutex.lock() = door unlocked, mutex.unlock() = door locked
-std::mutex barbershopDoor;
+std::mutex barbershopDoor;//дверь в барбершоп (исключение проблемы, когда 2 клиента могут сесть на одно место)
 
-std::queue<int> barbershopQueue = {};
+std::queue<int> barbershopQueue = {}; //эмитация зала ожидания
 
 void barber();
 void customer(int number);
@@ -37,38 +37,38 @@ int main() {
 void barber() {
 	while (true) {
 		if (barbershopQueue.size() == 0) {
-			std::cout << "Barber is sleeping" << std::endl;
+			std::cout << "Barber is sleeping" << std::endl; //Если в очереди/зале ожидания никого нет, то барбер спит
 		}
-		barberStatus.acquire();
-		if (barbershopQueue.size() > 0) {
-			int human = barbershopQueue.front();
+		barberStatus.acquire(); //пока барбер спит, он свободен,т.е его поток заблокирован
+		if (barbershopQueue.size() > 0) { // поток продолжается когда барбера разбудили, т.е пришел клиент
+			int human = barbershopQueue.front(); //приглашается человек, который дольше всего ждет (исключение проблемы starvation)
 			barbershopQueue.pop();
 			std::cout << "Barber is cutting hair for " << human << " customer" << std::endl;
 			
-			std::this_thread::sleep_for(std::chrono::seconds(3));
+			std::this_thread::sleep_for(std::chrono::seconds(3)); //процесс стрижки
 			std::cout << "Barber finished " << human << " customer" << std::endl;
-			barberRoomStatus.release();
+			barberRoomStatus.release(); //Клиент подстригся, комната освобождается
 		}
 	}
 }
 
 void customer(int number) {
-	std::this_thread::sleep_for(std::chrono::seconds(number));
-	barbershopDoor.lock();
-	if (barbershopQueue.size() < SEATS_NUMBER) {
-		barbershopQueue.push(number);
+	std::this_thread::sleep_for(std::chrono::seconds(number)); // клиент заходит через некоторое время. Представим, что время на самом деле случайное.
+	barbershopDoor.lock(); // клиент вошел в зал ожидания, открыл входную дверь. В это время никто больше не может войти, исключение проблемы, когда 2 клиента могут сесть на одно место
+	if (barbershopQueue.size() < SEATS_NUMBER) { // Если есть свободное место, то клиент становится в очередь/садится на свободный стул
+		barbershopQueue.push(number); 
 		std::cout << "Customer " << number << " is waiting in the queue" << std::endl;
-		barbershopDoor.unlock();
+		barbershopDoor.unlock(); // когда клиент сел входная дверь "освободилась" теперь следующий клиент сможет войти
 
-		barberRoomDoor.lock();
-		barberStatus.release();
-		barberRoomStatus.acquire();
+		barberRoomDoor.lock(); // Клиент пытается войти в комнату барбера.Если комната уже занята, то поток блокируется (посетитель ждет).
+		barberStatus.release(); // Когда клиент смог попасть в комнату, он "будит" барбера, если тот спал или просто садится в кресло
+		barberRoomStatus.acquire(); // Клиент занимает кресло
 		
 		std::cout << "Customer " << number << " has finished" << std::endl;
-		barberRoomDoor.unlock();
+		barberRoomDoor.unlock(); //Клиент выходит из комнаты барбера
 	}
 	else {
-		std::cout << "Customer " << number << " is leaving" << std::endl;
+		std::cout << "Customer " << number << " is leaving" << std::endl; // Если мест в зале ожидания нет, то клиент уходит
 		barbershopDoor.unlock();
 	}
 }
